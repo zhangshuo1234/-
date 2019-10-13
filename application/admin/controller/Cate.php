cb33e6798ff4d0f8746638ba0d9d92d34b1a1d1b
zhangshuo <1901933508@qq.com>
2019-10-13 16:44:00 +0800

    上传分类完成

---
+++ application/admin/controller/Cate.php
@@ -0,0 +1,33 @@
+<?php
+namespace app\admin\controller;
+use think\Controller;
+use think\Db;
+
+class Cate extends Controller
+{
+
+    public function product_category(){
+        return view();
+    }
+    public function add_product_category(){
+        if(request()->isGet()){
+            return view();
+        }
+        if(request()->isPost()){
+            //接值
+            $cate_name=request()->post("cate_name");     //分类名称
+            $cate_intro=request()->post("cate_intro"); //别名
+            $cate_keyword=request()->post("cate_keyword"); //关键字
+            $cate_desc=request()->post("cate_desc");//简单描述
+            $cate_order=request()->post("cate_order");//排序
+            $cate_pid=request()->post("cate_pid");
+            //存入数据库
+            $is=Db::name("cate")->insert(['cate_name'=>$cate_name,'cate_intro'=>$cate_intro,'cate_keyword'=>$cate_keyword,'cate_desc'=>$cate_desc,'cate_pid'=>$cate_pid,'cate_order'=>$cate_order]);
+            if($is){
+                $this->success("提交成功,请等待","cate/product_category");
+            }else{
+                $this->error("提交失败");
+            }
+        }
+    }
+}
--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -1,21 +1,14 @@
 <?php
 namespace app\admin\controller;
 
+use think\Db;
+
 class Index
 {
     public function index()
     {
         return view();
     }
-    public function product_category(){
-        return view();
-    }
-    public function add_product_category(){
-        if(request()->isGet()){
-            return view();
-        }
-        if(request()->isPost()){
-            request()->post("")
-        }
-    }
+
+
 }
